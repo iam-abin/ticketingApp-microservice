cb9d8cafdd3f61acf8f51f0ecdc065a0d4cb9bc4
Abin273 <abinvarghese273@gmail.com>
2023-08-30 20:49:50 +0530

    ingress running problems solveed

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,12 @@ import { json }from 'body-parser'
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentUser',(req,res)=>{
+    res.send("hi there")
+})
+
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000...");
     
-})
+}) 
